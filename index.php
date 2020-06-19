a3b7df7d8d434677b06925baea3567ad6283c403
walter6765jon <56731531+walter6765jon@users.noreply.github.com>
2020-06-19 18:17:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Location: https://www.sunrisepetlodge.com/pet-boarding/");
+header("Location: https://ns-new.herokuapp.com/");
 die();
 ?>
-https://www.sunrisepetlodge.com/pet-boarding/
+
